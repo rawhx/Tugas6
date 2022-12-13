486d15c1b312476347bc4290687ec49c08f8185e
rawhx_x <achmadhasbil24@gmail.com>
2022-12-14 06:11:04 +0700

    post

--- updateb.php
+++ updateb.php
@@ -1,11 +1,11 @@
 <?php
 include "koneksi.php";
 $id = $_POST['ID'];
-@$nama = $_POST['nama'];
-@$kelamin = $_POST['kelamin'];
-@$alamat = $_POST['alamat'];
-@$submit = $_POST['submit'];
-@$hasil = "UPDATE tabel_siswa SET Nama = '$nama', Jenis_Kelamin = '$kelamin', Alamat = '$alamat' WHERE ID=$id";
+$nama = $_POST['nama'];
+$kelamin = $_POST['kelamin'];
+$alamat = $_POST['alamat'];
+$submit = $_POST['submit'];
+$hasil = "UPDATE tabel_siswa SET Nama = '$nama', Jenis_Kelamin = '$kelamin', Alamat = '$alamat' WHERE ID=$id";
 if ($submit) {
     $simpan = mysqli_query($mysqli, $hasil);
     header('Location: index.php');
